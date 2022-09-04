ade1a26dab6d2df35df6cbda31b5ff3a34ed3381
lechi <lechidaitp@gmail.com>
2022-09-04 16:33:21 +0700

    Fix blade.php emmet

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -3,18 +3,27 @@
 
 -- you can even override default options here (core/options.lua)
 --
+
 --vim.cmd ":set shell='C:/Program\\ Files/PowerShell/7/pwsh.exe'"
 --vim.o.shell = '"C:/Program Files/PowerShell/7/pwsh.exe"'
+
+
+-- For blade.php file
+-- :set ft=blade.php.html
+
 vim.opt.clipboard = "unnamedplus"
 vim.opt.backup = false
 vim.opt.wb = false
 vim.opt.swapfile = false
 
-vim.cmd [[let g:neovide_fullscreen=v:true]]
-
-vim.cmd [[let g:neovide_remember_window_size = v:true]]
-vim.cmd [[let g:neovide_cursor_animation_length = 0.13]]
-vim.cmd [[let g:neovide_cursor_trail_length=0]]
+-- neovide stuff
+vim.g.neovide_cursor_animation_length = 0.05
+vim.g.neovide_cursor_trail_size = 0.1
+vim.g.neovide_cursor_vfx_mode = "ripple"
+vim.g.neovide_fullscreen= true
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_cursor_animation_length = 0.13
+vim.g.neovide_cursor_trail_length=0
 
 
 vim.opt.mouse = "a"
